d2f732cd968abfa18dc5569cb4d513c0c42d0416
Liam Mefus <liam.mefus@outlook.de>
2026-03-08 16:29:12 +0100

    for some reason using the best practice in the defineEventHandler is not enough

--- server/api/getAllLogEntries.ts
+++ server/api/getAllLogEntries.ts
@@ -1,7 +1,7 @@
 import { useDrizzle } from '#server/utils/drizzle'
 import { logEntriesTable } from '#server/db/schema'
 
-export default defineEventHandler(async (event) => {
+export default defineEventHandler(async () => {
   return useDrizzle()
     .select()
     .from(logEntriesTable)
